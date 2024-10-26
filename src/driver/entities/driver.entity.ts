f1fc557092c2fe8cc6cfec0cdfdc119977f1e47b
surajyadav484 <surajryadav20@gmail.com>
2024-10-26 14:22:50 +0530

    added driver entity

--- src/driver/entities/driver.entity.ts
+++ src/driver/entities/driver.entity.ts
@@ -1,1 +1,19 @@
-export class Driver {}
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Driver extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    nullable: false,
+  })
+  phoneNumber: string;
+
+  @Column()
+  profilePhotUrl: string;
+}
