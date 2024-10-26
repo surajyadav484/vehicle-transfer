5f3a658c3e721898df2147f0b97bb5adc8959c57
surajyadav484 <surajryadav20@gmail.com>
2024-10-26 11:02:35 +0530

    added vehicle entity

--- src/vehicle/entities/vehicle.entity.ts
+++ src/vehicle/entities/vehicle.entity.ts
@@ -1,4 +1,27 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class Vehicle {}
+export class Vehicle {
+  @PrimaryGeneratedColumn('uuid')
+  id: number;
+
+  @Column({
+    nullable: false,
+  })
+  vehicleNumber: string;
+
+  @Column({
+    nullable: false,
+  })
+  vehicleType: string;
+
+  @Column({
+    nullable: false,
+  })
+  pucCertificate: string;
+
+  @Column({
+    nullable: false,
+  })
+  insuranceCertificate: string;
+}
