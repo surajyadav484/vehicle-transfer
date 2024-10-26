a52933557bbc9aa08e487f998a5e99ac674e4756
surajyadav484 <surajryadav20@gmail.com>
2024-10-26 18:00:50 +0530

    bug fix

--- src/vehicle-transfer-history/dto/create-vehicle-transfer-history-input.dto copy.ts
+++ src/vehicle-transfer-history/dto/create-vehicle-transfer-history-input.dto copy.ts
@@ -1,11 +1,7 @@
-import { IsEnum } from 'class-validator';
-import { TransferType } from '../enums/type-enum';
-
 export class CreateVehicleTransferHistoryInputDto {
   vehicleId: string;
 
   transferredToId: string;
 
-  @IsEnum(TransferType, { message: 'Invalid transfer type' })
-  transferredToType: TransferType;
+  transferredToType: string;
 }
--- src/vehicle-transfer-history/dto/create-vehicle-transfer-history.dto.ts
+++ src/vehicle-transfer-history/dto/create-vehicle-transfer-history.dto.ts
@@ -1,10 +1,6 @@
-import { IsEnum } from 'class-validator';
 import { CreateVehicleTransferHistoryInputDto } from './create-vehicle-transfer-history-input.dto copy';
-import { TransferType } from '../enums/type-enum';
 
 export class CreateVehicleTransferHistoryDto extends CreateVehicleTransferHistoryInputDto {
   transferredFromId: string;
-
-  @IsEnum(TransferType, { message: 'Invalid transfer type' })
-  transferredFromType: TransferType;
+  transferredFromType: string;
 }
